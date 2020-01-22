3ccbbca2fb250ef07fdee30d92685fa61ce62a3e
kaustubh <kaustubhjaiswal221@gmail.com>
2020-01-22 15:09:30 +0530

    controller chnages

--- aura/SendMettingInviteCmp/SendMettingInviteCmpController.js
+++ aura/SendMettingInviteCmp/SendMettingInviteCmpController.js
@@ -1,5 +1,27 @@
 ({
-    myAction : function(component, event, helper) {
+    doSend : function(component, event, helper) {
+        var Email = component.get('v.email');
+        var startDate = component.get('v.startDate');
+        var duration = component.get('v.duration');
+        var datesplit = startDate.split('-');
+        var dateFormet = datesplit[0] + datesplit[1] + datesplit[2] ;
+         
+        var action = component.get('c.sendInvite');
+         action.setParams({
+            email:Email,
+            startDate:  dateFormet,
+            duration: duration
+         });
+         action.setCallback(this , function(response){
+               var state = response.getState();
+               alert(state);
+         });
+         $A.enqueueAction(action);
 
+         
+       
+
+       
+        
     }
 })
